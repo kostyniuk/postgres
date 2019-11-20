fb80cfd7435194353efd0e08ebdfdbf30bc8662c
kostyniuk <kostyniuk7@gmail.com>
2019-11-20 12:59:27 +0200

    both implementations working properly

---
+++ postgres-demo/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {Client} = require('pg');
+const client = new Client({
+  user: "postgres",
+  password: "Isco222000",
+  host: "localhost",
+  port: 5432,
+  database: "initial"
+});
+
+client.connect()
+  .then(() => console.log('Client connected'))
+  .then(() => client.query('SELECT * FROM person'))
+  .then(results => console.table(results.rows))
+  .catch(err => console.error(err))
+  .finally(() => client.end())
+
+/*
+
+const {Pool} = require('pg');
+const pool = new Pool({
+  user: "postgres",
+  password: "Isco222000",
+  host: "localhost",
+  port: 5432,
+  database: "initial"
+});
+
+pool.query("SELECT * FROM person", (error, result) => { console.table(result.rows)});
+*/
